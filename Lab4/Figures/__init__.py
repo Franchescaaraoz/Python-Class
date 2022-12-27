9ebd1a920f41b7f5d4b24c1febc0a745d6e04011
Franchescaaraoz <113604482+Franchescaaraoz@users.noreply.github.com>
2022-12-27 18:34:26 +0300

    Update __init__.py

--- Lab4/Figures/__init__.py
+++ Lab4/Figures/__init__.py
@@ -1,5 +1,5 @@
-from  circle import circle_area,circle_perimeter,default_radius
-from  triangle import triangle_area,triangle_perimeter,def_a,def_b,def_c
-from  square import square_area,square_perimeter,default_a
+from  circle import circle_area,circle_perimeter 
+from  triangle import triangle_area,triangle_perimeter
+from  square import square_area,square_perimeter
 
 
